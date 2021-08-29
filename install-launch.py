d90366e6a5750db2f78bb994368f3d735b2eb846
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-29 10:47:58 +0200

    Update install-launch.py

--- install-launch.py
+++ install-launch.py
@@ -3,3 +3,4 @@ from sys import platform as _platform
 if _platform == "linux" or _platform == "linux2":   
     os.system("pip install pynput")
 if _platform == "darwin":
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
