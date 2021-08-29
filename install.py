c70851b67446a37c6e12a5ef09676479702a08c8
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-29 10:50:40 +0200

    Update install.py

--- install.py
+++ install.py
@@ -2,5 +2,7 @@ import os
 from sys import platform as _platform
 if _platform == "linux" or _platform == "linux2":   
     os.system("pip install pynput")
-if _platform == "darwin":
+elif _platform == "darwin":
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+elif _platform == "win32" or _platform == "win64":
+    
